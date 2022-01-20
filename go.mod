6407c9161be5c4013cf07d281bf595265d687866
myml <wurongjie1@gmail.com>
2022-01-20 13:14:33 +0800

    test

---
+++ go.mod
@@ -0,0 +1,14 @@
+module action-sync
+
+go 1.17
+
+require github.com/google/go-github v17.0.0+incompatible
+
+require (
+	github.com/bradleyfalzon/ghinstallation v1.1.1 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/google/go-github/v29 v29.0.2 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/bradleyfalzon/ghinstallation"
+	"github.com/google/go-github/github"
+)
+
+func main() {
+	var privateKey string
+	var appID, installationID int64
+
+	flag.Int64Var(&appID, "app_id", 164400, "github app id")
+	flag.Int64Var(&installationID, "installation_id", 22221748, "github installation id")
+
+	flag.StringVar(&privateKey, "private_key", os.Getenv("private_key"), "")
+	flag.Parse()
+
+	log.Println("ok", privateKey[:10])
+	itr, err := ghinstallation.NewKeyFromFile(
+		http.DefaultTransport,
+		appID, installationID,
+		privateKey,
+	)
+	if err != nil {
+		panic(err)
+		// Handle error.
+	}
+	client := github.NewClient(&http.Client{Transport: itr})
+	client.Repositories.UpdateFile(context.Background(),
+		"peeweep-test", "test-action", "hello",
+		&github.RepositoryContentFileOptions{})
+	repos, _, err := client.Apps.ListRepos(context.Background(), &github.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	log.Println(repos)
+}
