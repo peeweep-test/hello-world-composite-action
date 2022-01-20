80115ecb1e8a6ac04aa6e37fd67e251110da4411
myml <wurongjie1@gmail.com>
2022-01-20 13:38:00 +0800

    test

--- main.go
+++ main.go
@@ -3,34 +3,49 @@ package main
 import (
 	"context"
 	"flag"
-	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/bradleyfalzon/ghinstallation"
 	"github.com/google/go-github/github"
 )
 
 func main() {
+	privateKey := []byte(os.Getenv("PRIVATE_KEY"))
+	repositor := os.Getenv("GITHUB_REPOSITORY")
 	var appID, installationID int64
-
 	flag.Int64Var(&appID, "app_id", 164400, "github app id")
 	flag.Int64Var(&installationID, "installation_id", 22221748, "github installation id")
-
 	flag.Parse()
 
-	itr, err := ghinstallation.New(http.DefaultTransport, appID, installationID, []byte(os.Getenv("PRIVATE_KEY")))
+	itr, err := ghinstallation.New(http.DefaultTransport, appID, installationID, []byte(privateKey))
 	if err != nil {
 		panic(err)
 	}
-
 	client := github.NewClient(&http.Client{Transport: itr})
-	client.Repositories.UpdateFile(context.Background(),
+	ctx := context.Background()
+	fileContent, _, resp, err := client.Repositories.GetContents(ctx, "peeweep-test", "test-action", "hello1", nil)
+	if err != nil {
+		if resp.StatusCode != http.StatusNotFound {
+			panic(err)
+		}
+	}
+	var sha string
+	if fileContent != nil {
+		sha = fileContent.GetSHA()
+	}
+	message := "chore: Sync by " + repositor
+	content := []byte(time.Now().String())
+	_, _, err = client.Repositories.UpdateFile(ctx,
 		"peeweep-test", "test-action", "hello",
-		&github.RepositoryContentFileOptions{})
-	repos, _, err := client.Apps.ListRepos(context.Background(), &github.ListOptions{})
+		&github.RepositoryContentFileOptions{
+			Message: &message,
+			Content: content,
+			SHA:     &sha,
+		},
+	)
 	if err != nil {
 		panic(err)
 	}
-	log.Println(repos)
 }
